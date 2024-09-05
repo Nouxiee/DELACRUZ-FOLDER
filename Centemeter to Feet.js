f26faecc95480aa899f947749c287903c6e22a5e
Nouxiee <elizerdelacruz19@gmail.com>
2024-09-05 18:56:04 +0800

    Delete Centemeter to Feet.js

--- Centemeter to Feet.js
+++
@@ -1,6 +0,0 @@
-//Centemeter to Feet.js
-function Centemeter to Feet(Centemeter) {
-    //1 Centemeter is approxiamate equal to 0.0328084
-let Centemeter= Feet * 0.0328084;
-return Centermeter.toFixed(2)
-}

de2e95f2c1965cc53678b8f1f224a98c6043c5eb
Nouxiee <elizerdelacruz19@gmail.com>
2024-09-05 20:13:44 +0800

    Add files via upload

--- Centimeter to Feet.js
+++ Centimeter to Feet.js
@@ -1,6 +1,4 @@
-// Centimeter to Feet converter
-function centimeterToFeet(centimeter) {
-    // 1 centimeter is approximately equal to 0.0328084 feet
+function centimeterToFeet(centimeter){
     let feet = centimeter * 0.0328084;
     return feet.toFixed(2);
   }

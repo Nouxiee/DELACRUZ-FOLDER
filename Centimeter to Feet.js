541fe605189381665430e7c097aafd4c24456b15
Nouxiee <elizerdelacruz19@gmail.com>
2024-09-09 13:03:30 +0800

    Add files via upload

--- Centimeter to Feet.js
+++ Centimeter to Feet.js
@@ -1,4 +1,17 @@
-function centimeterToFeet(centimeter){
+function centimeterToFeet(centimeter) {
     let feet = centimeter * 0.0328084;
     return feet.toFixed(2);
-  }
+}
+
+function compute() {
+    let centimeter = parseFloat(document.getElementById("Centimeter").value);
+    let feet = centimeterToFeet(centimeter);
+    document.getElementById("Feet").value = feet;
+    document.getElementById("Feet").disabled = true;
+}
+
+function clear() {
+    document.getElementById("Centimeter").value = "";
+    document.getElementById("Feet").value = "";
+    document.getElementById("Feet").disabled = false;
+}
